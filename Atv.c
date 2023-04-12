4a96d5308680a300c7376abe4739a1523174107a
Leojjackson <leojjackson@outlook.com>
2023-04-12 20:56:20 -0300

    atv2 add

--- Atv.c
+++ Atv.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 
-int main()
-{
+// int main()
+// {
     // Atividade 1
-    // 
-    int a,b,x;
+    // ler 2 valores inteiros A e B e atribui o resultado da soma a variavel X;
+    // int a,b,x;
 
-    scanf("%d", &a);
-    scanf("%d", &b);
-    x = a+b;
+    // scanf("%d", &a);
+    // scanf("%d", &b);
+    // x = a+b;
 
-    printf("SOMA = %d", x);
+    // printf("X = %d", x);
 
-    printf("Fim do programa");
+    // printf(" \n Fim do programa");
 
-    return 0;
-}
+    // fim do primeiro programa
+
+//     return 0;
+// }
+//---------------------------------------
+
+// int main()
+// {
+    //Atividade 2
+    //a formula para calcular a circunferencia é: area = TT.raio². considerando para este problema que tt = 3.14159.
+    // efetue o calculo da area. elevando o valor de raio ao quadrado e multiplicando  por tt.
+    
+//     double raio, area,a;
+//     area = 3.14159;
+//     scanf("%lf", & raio);
+//     a = area * (raio * raio);
+
+//     printf("A=%lf", a);
+//     printf("Fim do programa");
+//     return 0;
+
+    
+// }
+
+//---------------------------------------
