fc6c1579ce6f14d7e547bb77eb41de292f3f1b0a
Leojjackson <leojjackson@outlook.com>
2023-04-12 20:45:31 -0300

    atv1 add

--- Atv.c
+++ Atv.c
@@ -2,6 +2,17 @@
 
 int main()
 {
-    
+    // Atividade 1
+    // 
+    int a,b,x;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    x = a+b;
+
+    printf("SOMA = %d", x);
+
+    printf("Fim do programa");
+
     return 0;
 }
