b53248005651348c09e6535e883a1e057e252c52
Leojjackson <leojjackson@outlook.com>
2023-04-13 08:45:12 -0300

    Atividade  8 add

--- Atv.c
+++ Atv.c
@@ -3,6 +3,7 @@
 // int main()
 // {
     // Atividade 1
+    
     // ler 2 valores inteiros A e B e atribui o resultado da soma a variavel X;
     // int a,b,x;
 
@@ -23,6 +24,7 @@
 // int main()
 // {
     //Atividade 2
+
     //a formula para calcular a circunferencia é: area = TT.raio². considerando para este problema que tt = 3.14159.
     // efetue o calculo da area. elevando o valor de raio ao quadrado e multiplicando  por tt.
     
@@ -43,6 +45,7 @@
 // int main()
 // {
     //Atividade 3
+
     //Leia 2 valores inteiros A, B  a seguir calcule o valor entre elas e atribua o resultasdo a variavel SOMA.
 
 //     int a, b, soma;
@@ -62,6 +65,7 @@
 // int main()
 // {
     //Atividade 4
+
     //leia 2 valores inteiros, A seguir calcule o produto entre esses dois valores e atribua esta operação a variavel PROD.
 
 //     int valor1,valor2,prod;
@@ -81,6 +85,7 @@
 // int main()
 // {
     //Atividade 5
+
     //leia dois valores flutuantes, que correspondem a notas de alunos. a seguir calcule a média do aluno, sabendo que a nota A tem peso 3.5.
     //a nota B tem peso 7.5(A soma dos pesos portanto é 11)Assuma que cada nota pode ir de 0.0 até 10.0.
 
@@ -103,6 +108,7 @@
 // int main()
 // {
     //Atividade 6
+
     //leia 3 valores A,B,C que são 3 notas de aluno.calcule a MEDIA do aluno, a nota A tem peso 2, anota B tem peso 3 e a nota C tem peso 5.
 
 //     double a;
@@ -129,6 +135,7 @@
 // int main()
 // {
     //Atividade 7
+
     //leia 4 valores inteiros A, B, C, D a seguir calcule a diferença Do produto A e B pelo produto de C e D.
 
 //     int a,b,c,d,diferenca;
@@ -146,6 +153,34 @@
 //     printf("DIFERENCA = %d", diferenca);
 
 //     printf("\n Fim do programa");
+//     return 0;
+// }
+
+//---------------------------------------
+
+// int main()
+// {
+    //Atividade 8
+
+    //Escreva um programa que leia o Numero de um funcionario, o salario por hora, valor q recebe por hora
+    // e calcula o salario desse funcionario;
+
+//     int id;
+//     double Salario_hora;
+//     double Valor_hora;
+//     double salario;
+
+//     scanf("%d", &id);
+//     scanf("%lf", &Salario_hora);
+//     scanf("%lf", &Valor_hora);
+
+//     salario = Salario_hora * Valor_hora;
+
+//     printf("NUMBER = %d \n", id);
+//     printf("SALARY = U$ %lf", salario);
+//     printf("\n Fim do programas");
+
+
 //     return 0;
 // }
 
